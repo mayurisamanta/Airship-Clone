ba7f9378c1d48b398baad4f211442011ad83417e
Rakshikakadu <rakshikakadu@gmail.com>
2022-07-21 00:48:13 +0530

    added navbars

---
+++ Components/navbar.js
@@ -0,0 +1,18 @@
+let navbar1 = ()=>{
+    return ` <div>
+    <a href="projectProfilePage.html">
+        <img src="https://go.airship.com/assets/75ac4e3598fc7916f372c3b5cb3599c7b5fa2ec0/images/airship-logo-horizontal.svg" alt="Error">
+    </a>
+
+</div>
+
+    <div>
+        <a href="projectProfilePage.html"><h4>All Projects</h4></a>
+    </div>
+
+<div>
+    <img class="icon" src="https://cdn-icons-png.flaticon.com/128/3916/3916709.png" alt="Error">
+    <img class="icon" src="https://cdn-icons-png.flaticon.com/128/3917/3917711.png" alt="Error">
+</div>`
+}
+export {navbar1}
