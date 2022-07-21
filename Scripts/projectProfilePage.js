6f2258f309a430b6c9143e1b8716986f950ebfcf
Rakshikakadu <rakshikakadu@gmail.com>
2022-07-21 17:51:41 +0530

    Project Form

--- Scripts/projectProfilePage.js
+++ Scripts/projectProfilePage.js
@@ -1,3 +1,22 @@
-import { navbar1 } from "../Components/navbar.js";
+import { navbar1 , navbar2 } from "../Components/navbar.js";
 
 document.querySelector("#navbar1").innerHTML=navbar1();
+document.querySelector("#navbar2").innerHTML=navbar2();
+
+// let count=0;
+
+// onclick function(){
+//     count++;
+//     if(count==1){
+//         Display the result
+//     }else{
+//         Display result on another click
+//         count=0;
+//     }
+// }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+
+ 
