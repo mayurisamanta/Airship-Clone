06f34793e4a86221858e41b8e3cb2210c858b894
mayurisamanta <samantamayuri@gmail.com>
2022-07-23 15:21:56 +0530

    complete

--- login.js
+++ login.js
@@ -5,26 +5,28 @@ document.getElementById("logo").addEventListener("click",()=>{
 let data = JSON.parse(localStorage.getItem("data"));
 document.getElementById("submit").addEventListener("click", (event)=>{
     event.preventDefault();
-    let form = document.getElementById("formdiv");
-    let e = form.email.value;
-    let p = form.password.value;
-
+    let e = document.getElementById("email").value;
+    let p = document.getElementById("password").value;
+    let flag = true;
     for (let i = 0; i < data.length; i++){
         if (data[i].email == e && data[i].password == p){
+            
+            let obj = {
+                email : e,
+                password : p
+            }
+            localStorage.setItem("login", JSON.stringify(obj));
+            window.location.href = "createProjectForm.html";
+            return;
+        }
+        if (flag) {
             let p = document.createElement("p");
-            p.innerText = "This company name is already taken. Please choose another.";
+            p.innerText = "The username and password combination is invalid.";
             p.style.border = "1px solid #ffb700";
             p.style.padding = "5px";
-            document.getElementById("companyerr").append(p);
-            return;        
+            let div = document.getElementById("companyerr");
+            div.innerHTML = null;
+            div.append(p);
         }
     };
-    let obj = {
-        firstname : f,
-        lastname : l,
-        email : e,
-        company : c,
-    }
-    data.push(obj);
-    localStorage.setItem("data", JSON.stringify(data));
 });
